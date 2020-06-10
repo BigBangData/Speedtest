f6c4d563db307fb36f41881e8b6de0b0f8955460
Marcelo Sanches <sanches.cello@gmail.com>
2020-06-09 21:27:05 -0600

    Reformat json; Add computer name.

--- speedtest.py
+++ speedtest.py
@@ -6,6 +6,7 @@ import random
 import subprocess
 from datetime import datetime
 
+
 def get_speedtest_json():
     """Conducts a single speedtest and returns a json
     with the results.
@@ -24,11 +25,11 @@ def get_speedtest_json():
     try:
         _json = stdout.decode('utf-8')
         _dict = json.loads(_json)
-    except json.decoder.JSONDecodeError as e:
-        print(e)
-        pass
-    
-    return(_dict)
+        return(_dict)
+    except (JSONDecodeError, UnboundLocalError, ValueError):
+        _dict = {}
+        return(_dict)
+
 
 def extract_speeds(_dict):
     """Given a dictionary of results from the speedtest,
@@ -41,6 +42,7 @@ def extract_speeds(_dict):
     
     # return download an upload speeds as a tuple
     return(download, upload)
+
 
 def collect_speeds(iters, mins):
     """Given a number of loop iterations and minutes of wait time
@@ -96,18 +98,67 @@ def collect_speeds(iters, mins):
     return(_dict)
 
 
+def extract_filename():
+    """Creates a filename by extracting the next name from
+    filenames in data/.
+    """
+    
+    # gets list of files and revers it using a natural key
+    # this sorts correctly when going from 1 to 2+ digits
+    filelist = os.listdir('data/')
+    filelist.sort(key=lambda x: int(re.sub('\D', '', x))
+                  , reverse=True)
+    
+    # get last digit and add one, create filename
+    lastdigit = int(filelist[0].split('.')[0][4:6])+1
+    filename = ''.join(['test', str(lastdigit), '.json'])
+    
+    return(filename)
+
+
+def extract_machineinfo():
+    """Use subprocess to pass a wmic command to extract 
+    the machine name and info.
+    """
+    
+    try:
+        out = subprocess.Popen(['wmic', 'csproduct', 'get', 'name'],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT)
+        stdout, stderr = out.communicate()
+        
+        # cleanup name
+        names = stdout.decode('utf-8').split('\n')[1].split(' ')
+        remove = ['', '\r\r', '\r', '\n']
+        clean_names = [x for x in names if x not in remove]
+        name = ' '.join([str(x) for x in clean_names]) 
+    
+        return(name)
+    
+    except (JSONDecodeError, UnboundLocalError, ValueError):
+        name = 'No Name'
+        
+        return(name)
+    
+
 if __name__=='__main__':
     
+    # Make sure to git pull since filename is obtained from data/ 
+    print("Make sure to git pull before proceeding.\n")
+    
     # get user input 
-    # [TODO: sanitize, error check]
+    # [TODO: sanitize, error check]	
     location = input("Enter room name: ") 
-    filename = input("Enter file name: ")
     iters = int(input("Enter number of speed tests: "))
     mins = int(input("Enter max waittime (mins) between tests: "))
+
+    # extract computer name
+    computer = extract_machineinfo()
     
-    # collect data, add location
+    # collect data, add location, computer name
     _dict = collect_speeds(iters, mins)
     _dict['location'] = location
+    _dict['computer'] = computer
     
     print('Writing out json.')
     
@@ -116,9 +167,12 @@ if __name__=='__main__':
     if not os.path.exists(_dir):
         os.makedirs(_dir)
         
+    # get filename
+    filename = extract_filename()
+    
     # save to data dir
-    filepath = ''.join([_dir, str(filename), '.json'])
+    filepath = ''.join([_dir, str(filename)])
     with open(filepath, 'w') as f:
         json.dump(_dict, f, indent=4)
                
-    print('Json file saved. Exiting now...')    
+    print('Json file saved. Exiting now...')
