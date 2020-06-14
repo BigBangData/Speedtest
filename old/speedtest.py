5532564f8c367318647c6ddcfb237db5bf54f098
Marcelo Sanches <sanches.cello@gmail.com>
2020-06-14 15:14:45 -0600

    Replace speedtest.py; start version 2

--- old/speedtest.py
+++ old/speedtest.py
@@ -28,25 +28,7 @@ def get_speedtest_json():
         return(_dict)
     except (json.decoder.JSONDecodeError, UnboundLocalError, ValueError) as e:
         print(e, flush=True)
-        
-        # use a default dict
-        _dict = {
-                    "day": "2020-06-01",
-                    "data": {
-                        "time": [
-                            "00:00:00"
-                        ],
-                        "download": [
-                            0
-                        ],
-                        "upload": [
-                            0
-                        ]
-                    },
-                    "location": "None",
-                    "computer": "None"
-                }
-        
+        _dict = {}
         return(_dict)
 
 
@@ -63,15 +45,88 @@ def extract_speeds(_dict):
     return(download, upload)
 
 
-def extract_ping(_dict):
-    """Given a dictionary of results from the speedtest,
-    perform necessary transformations to return ping stats.
+def collect_speeds(iters, mins):
+    """Given a number of loop iterations and minutes of wait time
+    variations between each iteration, perform and collect speedtest
+    results, dates, and times.
     """
     
-    jitter = _dict['ping']['jitter']
-    latency = _dict['ping']['latency']
+    # instantiate lists
+    time_list = []
+    upload_list = []
+    download_list = []
+    
+    for i in range(iters):
+        
+        # wait random num secs for up to mins
+        secs = random.randint(1, 60*mins)
+        print('Loop ' + str(i+1) + ': waiting for ' \
+              + str(secs) + ' secs...', flush=True)
+        time.sleep(secs)
+        
+        # collect speeds
+        print('Performing speed test no.' + str(i+1), flush=True)
+        _dict = get_speedtest_json()
+        
+        if not bool(_dict):          
+            # create a default dict to avoid empty dict errors
+            print('ERROR: dictionary is empty.\nCreating default dict.')
+            _dict = {
+                        "day": "2020-06-01",
+                        "data": {
+                            "time": [
+                                "00:00:00"
+                            ],
+                            "download": [
+                                0
+                            ],
+                            "upload": [
+                                0
+                            ]
+                        },
+                        "location": "None",
+                        "computer": "None"
+                    }
 
-    return(jitter, latency)
+            download, upload = 0, 0
+
+        else:
+
+            # extract speeds
+            download, upload = extract_speeds(_dict)
+        
+        print('Download speed: ' + str(download) + ' Mbps', flush=True)
+        
+        # add to lists
+        print('Adding to speed lists.', flush=True)
+        
+        download_list.append(download)
+        upload_list.append(upload)
+        
+        # get dates and times
+        now = time.time()
+        dt_obj = datetime.fromtimestamp(now)
+        
+        # transform into readable format
+        _day, _time = str(dt_obj).split(' ')
+        _time = _time.split('.')[0]  
+        
+        # add to lists
+        print('Adding to time list.', flush=True)
+        
+        time_list.append(_time)
+        
+    # gather up lists into a final dict
+    final_dict = {
+                  'day':_day,
+                  'data':{
+                          'time':time_list,
+                          'download':download_list,
+                          'upload':upload_list
+                         }
+                 }       
+    
+    return(final_dict)
 
 
 def extract_filename():
@@ -116,68 +171,6 @@ def extract_machineinfo():
         
         return(name)
     
-    
-def collect_info():
-    """Perform 10 speed tests with up to 5 mins of wait time 
-    between them and collect results.
-    """
-    
-    # instantiate lists
-    time_list = []
-    jitter_list = []
-    latency_list = []
-    upload_list = []
-    download_list = []
-    
-    for i in range(3):
-        
-        # wait random num secs for up to 5 mins
-        #secs = random.randint(10, 300)
-        secs = 1
-        print('Loop ' + str(i+1) + ': waiting for ' \
-              + str(secs) + ' secs...')
-        time.sleep(secs)
-        
-        print('Performing speed test no.' + str(i+1))
-        _dict = get_speedtest_json()
-        
-        # collect data
-        download, upload = extract_speeds(_dict)
-        jitter, latency = extract_ping(_dict)
-        
-        # add to lists
-        print('Adding to lists.')
-        jitter_list.append(jitter)
-        latency_list.append(latency)
-        download_list.append(download)
-        upload_list.append(upload)
-    
-        # get dates and times
-        now = time.time()
-        dt_obj = datetime.fromtimestamp(now)
-        
-        # transform into readable format
-        _day, _time = str(dt_obj).split(' ')
-        _time = _time.split('.')[0]  
-
-        # add to lists
-        print('Adding to time list.')
-        time_list.append(_time)
-        
-    # gather up lists into a dict
-    _dict = {
-             'day':_day,
-             'data':{
-                     'time':time_list,
-                     'jitter':jitter_list,
-                     'latency':latency_list,
-                     'download':download_list,
-                     'upload':upload_list
-                    }
-            }       
-    
-    return(_dict)
-
 
 if __name__=='__main__':
     
@@ -187,25 +180,19 @@ if __name__=='__main__':
     # get user input 
     # [TODO: sanitize, error check]	
     location = input("Enter room name: ") 
-    location = location.strip()
+    iters = int(input("Enter number of speed tests: "))
+    mins = int(input("Enter max waittime (mins) between tests: "))
 
     # extract computer name
     computer = extract_machineinfo()
     
     # collect data
-    _dict = collect_info()
+    _dict = collect_speeds(iters, mins)
     
     # add location, computer name
     _dict['location'] = location
     _dict['computer'] = computer
     
-    # reorder dict
-    keyorder = ['day', 'location', 'computer', 'data']
-
-    final_dict = dict() 
-    for key in keyorder: 
-        final_dict[key] = _dict[key]
-
     print('Writing out json.', flush=True)
     
     # make data dir if not exists
@@ -219,6 +206,6 @@ if __name__=='__main__':
     # save to data dir
     filepath = ''.join([_dir, str(filename)])
     with open(filepath, 'w') as f:
-        json.dump(final_dict, f, indent=4)
+        json.dump(_dict, f, indent=4)
                
     print('Json file saved. Exiting now...', flush=True)
