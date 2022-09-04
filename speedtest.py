03738ce1ea2ba76a872eff051d59189736a43e22
Marcelo Sanches <sanches.cello@gmail.com>
2022-09-04 13:08:58 -0600

    Start py script revision for new gateway and APs

--- speedtest.py
+++ speedtest.py
@@ -7,40 +7,53 @@ import subprocess
 from datetime import datetime
 
 
-def get_speedtest_json():
-    """Conducts a single speedtest and returns a json
-    with the results.
+def get_datetime():
+    # get dates and times
+    now = time.time()
+    dt = datetime.fromtimestamp(now)
+    
+    # transform into readable format
+    day_, time_ = str(dt).split(' ')
+    time_ = time_.split('.')[0]
+
+    return day_, time_
+
+
+def get_speedtestjson_():
+    """Conducts one speedtest and returns JSON with results.
     """
     
     # pass a subprocess command to cmd to get json
-    try:
-        out = subprocess.Popen(['speedtest.exe', 
-                                '-f', 'json'],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT)
-        stdout, stderr = out.communicate()
-    except:
-        pass
-    
+    # todo: add try/except block after specific error
+    out = subprocess.Popen(
+        ['speedtest.exe',
+        '-f', 'json'],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT
+    )
+    stdout, stderr = out.communicate()
+
     # decode bytes and transform json into a dict
     try:
-        _json = stdout.decode('utf-8')
-        _dict = json.loads(_json)
-        return(_dict)
-    except (json.decoder.JSONDecodeError, 
-            UnboundLocalError, ValueError) as e:
+        json_ = stdout.decode('utf-8')
+        dict_ = json.loads(json_)
+        return(dict_)
+    except (json.decoder.JSONDecodeError,
+        UnboundLocalError, ValueError) as e:
         print(e, flush=True)
         
         # use a default dict
-        _dict = {
+        day_, time_ = get_datetime()
+
+        dict_ = {
                   "test": 0,
-                  "day": "2020-06-01",
+                  "day": day_,
                   "location": "None",
                   "computer": "None",
-                  "router_location": "None",
+                  "access_point": "None",
                   "data": {
                       "time": [
-                          "00:00:00"
+                          time_
                       ],
                       "jitter": [
                           0
@@ -57,43 +70,43 @@ def get_speedtest_json():
                   }
               }
         
-        return(_dict)
+        return(dict_)
 
 
-def extract_speeds(_dict):
+def extract_speeds(dict_):
     """Given a dictionary of results from the speedtest,
-    perform necessary transformations to return speeds.
+       perform necessary transformations to return speeds.
     """
     
     # extract download and upload speeds in Mbps
-    download = _dict['download']['bytes']/1e6
-    upload = _dict['upload']['bytes']/1e6
+    download = dict_['download']['bytes']/1e6
+    upload = dict_['upload']['bytes']/1e6
     
     # return download an upload speeds as a tuple
     return(download, upload)
 
 
-def extract_ping(_dict):
+def extract_ping(dict_):
     """Given a dictionary of results from the speedtest,
-    perform necessary transformations to return ping stats.
+       perform necessary transformations to return ping stats.
     """
     
-    jitter = _dict['ping']['jitter']
-    latency = _dict['ping']['latency']
+    jitter = dict_['ping']['jitter']
+    latency = dict_['ping']['latency']
 
     return(jitter, latency)
 
 
 def extract_filename():
-    """Creates a filename by extracting the next name from
-    filenames in data/.
+    """Create a filename by extracting the next name from
+       filenames in data/.
     """
     
     # gets list of files and reverses it using a natural key
     # this sorts correctly when going from 1 to 2+ digits
     filelist = os.listdir('data/')
     filelist.sort(key=lambda x: int(re.sub('\D', '', x))
-                  , reverse=True)
+        , reverse=True)
     
     # get last digit and add one, create filename
     lastdigit = int(filelist[0].split('.')[0][4:6])+1
@@ -108,10 +121,12 @@ def extract_machineinfo():
     """
     
     try:
-        out = subprocess.Popen(['wmic', 'csproduct', 
-                                'get', 'name'],
-                                stdout=subprocess.PIPE,
-                                stderr=subprocess.STDOUT)
+        out = subprocess.Popen(
+            ['wmic', 'csproduct', 
+            'get', 'name'],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.STDOUT
+        )
         stdout, stderr = out.communicate()
         
         # cleanup name
@@ -119,90 +134,74 @@ def extract_machineinfo():
         remove = ['', '\r\r', '\r', '\n']
         clean_names = [x for x in names if x not in remove]
         name = ' '.join([str(x) for x in clean_names]) 
-    
+        
         return(name)
-    
+        
     except (JSONDecodeError, UnboundLocalError, ValueError):
         name = 'No Name'
         
         return(name)
     
     
-def collect_info(iters=10, mins=5):
-    """Perform iters speed tests with up to mins mins of wait 
-    time between them and collect results. 
-
-    Defaults to 10 tests for upto 5 mins of wait time.
-    """
+def collect_info(iters=2, mins=1):
+    """Perform `iters` speed tests with up to `mins` minutes
+    of wait time between tests and collect results into a dict.
     
-    # instantiate lists
-    time_list = []
-    jitter_list = []
-    latency_list = []
-    upload_list = []
-    download_list = []
+    Defaults to 10 tests for up-to 5 mins of wait time.
+    """
+
+    times, jitters, latencies = [], [], []
+    downloads, uploads = [], []
     
     for i in range(iters):
         
-        # wait random num of secs for up to 5 mins
+        # wait a pseudo-random amt of time
         secs = random.randint(10, 60*mins)
-
-        print('Loop ' + str(i+1) + ': waiting for ' \
-              + str(secs) + ' secs...')
+        print(f'Test {i+1} | Waiting {secs}s...')
         time.sleep(secs)
-        
-        print('Performing speed test no.' + str(i+1))
-        _dict = get_speedtest_json()
-        
-        # collect data
-        download, upload = extract_speeds(_dict)
-        jitter, latency = extract_ping(_dict)
-        
-        # add to lists
-        print('Adding to lists.')
-        jitter_list.append(jitter)
-        latency_list.append(latency)
-        download_list.append(download)
-        upload_list.append(upload)
-    
-        # get dates and times
-        now = time.time()
-        dt_obj = datetime.fromtimestamp(now)
-        
-        # transform into readable format
-        _day, _time = str(dt_obj).split(' ')
-        _time = _time.split('.')[0]  
 
-        # add to time list
-        time_list.append(_time)
-        
-    # gather up lists into a dict
-    _dict = {
-             'day':_day,
+        # perform the test
+        print(f'Test {i+1} | Reaching out to speedtest.net...')
+        dict_ = get_speedtestjson_()
+        day_, time_ = get_datetime()
+        jitter, latency = extract_ping(dict_)
+        download, upload = extract_speeds(dict_)
+
+        # gather the data into lists
+        print(f'Test {i+1} | Gathering data...')
+        times.append(time_)
+        jitters.append(jitter)
+        latencies.append(latency)
+        downloads.append(download)
+        uploads.append(upload)
+
+    # then into a dict
+    dict_ = {
+             'day':day_,
              'data':{
-                     'time':time_list,
-                     'jitter':jitter_list,
-                     'latency':latency_list,
-                     'download':download_list,
-                     'upload':upload_list
+                     'time': times,
+                     'jitter': jitters,
+                     'latency': latencies,
+                     'download': downloads,
+                     'upload': uploads
                     }
-            }       
+            }
     
-    return(_dict)
+    return(dict_)
 
 
 if __name__=='__main__':
     
     # git pull since filename is obtained from data/ 
-    print("Make sure to git pull before proceeding.\n")
+    print("Make sure to `git pull` before proceeding.\n")
     
     # get user input 
-    # [TODO: sanitize, error check]
-    location = input("Enter room name: ")
+    # todo: sanitize, error check, use args
+    location = input("Enter room tested: ")
     location = location.lower().strip()
     
-    router_loc = input("Enter router location: ")
-    router_loc = router_loc.lower().strip()
+    access_point = input("Enter access point: ")
+    access_point = access_point.lower().strip()
 
     # extract computer name automagically
     computer = extract_machineinfo()
@@ -211,37 +210,36 @@ if __name__=='__main__':
     _dir = 'data/'
     if not os.path.exists(_dir):
         os.makedirs(_dir)
-       
+    
     # get lastdigit and filename
     lastdigit,filename = extract_filename()
     
-    # collect data using defaults 
-    # [TODO: try params to specify defaults]
-    _dict = collect_info()
+    # collect data using defaults
+    dict_ = collect_info()
  
     # add location, computer name
-    _dict['location'] = location
-    _dict['computer'] = computer
-    _dict['router_location'] = router_loc
-    _dict['test'] = lastdigit
+    dict_['location'] = location
+    dict_['computer'] = computer
+    dict_['access_point'] = access_point
+    dict_['test'] = lastdigit
     
     # reorder dict
     keyorder = ['test'
                 ,'day'
                 ,'location'
                 ,'computer'
-                ,'router_location'
+                ,'access_point'
                 ,'data']
 
-    final_dict = dict() 
+    finaldict_ = dict() 
     for key in keyorder: 
-        final_dict[key] = _dict[key]
+        finaldict_[key] = dict_[key]
 
-    print('Writing out json.', flush=True)
+    print('Saving all test data...', flush=True)
     
     # save to data dir
     filepath = ''.join([_dir, str(filename)])
     with open(filepath, 'w') as f:
-        json.dump(final_dict, f, indent=4)
-               
-    print('Json file saved. Exiting now...', flush=True)
+        json.dump(finaldict_, f, indent=4)
+
+    print(f'Data saved. Check {filepath}', flush=True)
